dd75bfbad80f2b1d8273c26b33f2f14189cb3108
Erezana <erzanaberisha2001@gmail.com>
2024-01-15 02:09:26 +0100

    added search

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Search from './components/search_component/search'
 
 function App() {
+  const handleOnSearchChange =(searchData) =>{
+    console.log(searchData);
+  }
   return (
-    <div className="App">
-        
+    <div className="container">
+     <div className="container_box">
+        <Search onSearchChange={handleOnSearchChange}/>
+     </div>
     </div>
   );
 }
---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '02b9eb3744mshe95129a5e7ab5d1p15da62jsn524acf257deb',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo"
