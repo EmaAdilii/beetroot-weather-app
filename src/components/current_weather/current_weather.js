f9653b6f31c79e7db68eb06a8df906a72544f1ea
Erezana <erzanaberisha2001@gmail.com>
2024-01-18 01:30:08 +0100

    added bootstrap to make it responsive

--- src/components/current_weather/current_weather.js
+++ src/components/current_weather/current_weather.js
@@ -2,53 +2,59 @@ import "./current_weather.css";
 
 const CurrentWeather = ({ data }) => {
   return (
-    <div className="weather">
-      <div className="weather_box">
-        <div className="weather_box_title">
-          <h1>Current Weather</h1>
-        </div>
-        <div className="weather_box_content">
-          <img alt="sunny" src={`./icons/${data.weather[0].icon}.png`}/>
-          <p className="weather_box_content_temperature">{` ${Math.round(data.main.temp)}°C`}</p>
-          <p className="weather_box_content_temperature_description">{data.weather[0].description}</p>
-          <p className="weather_box_content_city">
+   
+      <div className="row justify-content-center">
+        <div className="col-12 col-md-6 mx-auto custom-box text-center d-flex align-items-center flex-column details">
+          <h1 className="mb-4">Current Weather</h1>
+          <img src={`./icons/${data.weather[0].icon}.png`} alt="Weather Icon" className="mx-auto d-block mb-4" />
+          <div className="custom-box-details">
+            <p className="temperature">{`Temperature: ${Math.round(data.main.temp)}°C`}</p>
+            <p>{data.weather[0].description}</p>
+            <p className="weather_box_content_city">
             <i className="fa-solid fa-location-dot"></i> {data.city}</p>
-        </div>
-        <div className="weather_box_details">
+            
+          </div>
+          <div className="weather_box_details">
           <div className="weather_box_details_feelsLike box">
             <i className="fa-solid fa-temperature-quarter"></i>
             <div>
-              <p>{`${Math.round(data.main.feels_like)}°C`}</p>
-              <p>feels like</p>
+              <p className="weather_box_details_p">{`${Math.round(data.main.feels_like)}°C`}</p>
+              <p className="weather_box_details_p">feels like</p>
             </div>
           </div>
           <div className="weather_box_details_wind box">
             <i className="fa-solid fa-wind"></i>
             <div>
-              <p>{data.wind.speed}</p>
-              <p>wind</p>
+              <p className="weather_box_details_p">{data.wind.speed}</p>
+              <p className="weather_box_details_p">wind</p>
             </div>
           </div>
           <div className="weather_box_details_humidity box">
-            <span className="material-symbols-outlined">
+            <span className="material-symbols-outlined weather_box_details_p ">
               {" "}
               humidity_percentage{" "}
             </span>
             <div>
-              <p>{`${data.main.humidity}%`}</p>
-              <p>humidity</p>
+              <p className="weather_box_details_p">{`${data.main.humidity}%`}</p>
+              <p className="weather_box_details_p">humidity</p>
             </div>
           </div>
           <div className="weather_box_details_pressure box">
             <span className="material-symbols-outlined">compress</span>
             <div>
-              <p>{`${data.main.pressure} hPa`}</p>
-              <p>Pressure</p>
+              <p className="weather_box_details_p">{`${data.main.pressure} hPa`}</p>
+              <p className="weather_box_details_p">Pressure</p>
             </div>
           </div>
+
+          
+            
+          </div>
+          
         </div>
       </div>
-    </div>
+  
   );
 };
+
 export default CurrentWeather;
--- src/components/search_component/search.js
+++ src/components/search_component/search.js
@@ -1,60 +1,42 @@
-import { useState, useEffect } from "react";
-import { AsyncPaginate } from "react-select-async-paginate";
-import { GEO_API_URL, geoApiOptions, WEATHER_API_URL, weatherApiOptions } from "../../api";
+import { useState } from "react"
+import {AsyncPaginate} from "react-select-async-paginate"
+import { GEO_API_URL, geoApiOptions } from "../../api";
 
-const Search = ({ onSearchChange }) => {
-  const [search, setSearch] = useState(null);
-  const [defaultLocation, setDefaultLocation] = useState(null);
+const Search =({onSearchChange}) =>{
+    const [search, setSearch] = useState(null);
+    const loadOptions =(inputData) =>{
+        
 
-  useEffect(() => {
-    // Get the user's current location using browser geolocation API
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
-          const { latitude, longitude } = position.coords;
-          setDefaultLocation({ latitude, longitude });
-        },
-        (error) => {
-          console.error("Error getting current location:", error.message);
+	return  fetch(
+       `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputData}` , geoApiOptions)
+	.then((response) => response.json()) 
+	.then((response)=>{
+        return{
+            options: response.data.map((city) =>{
+               return{
+                value: `${city.latitude} ${city.longitude}`,
+                label: `${city.name}, ${city.countryCode}`,
+               
+               } 
+            })
         }
-      );
-    } else {
-      console.error("Geolocation is not supported by this browser.");
+    })
+    .catch((err) => console.log(err));
+
+    };
+    const handleOnChange = (searchData) =>{
+        setSearch(searchData);
+        onSearchChange(searchData);
     }
-  }, []); // Empty dependency array ensures the effect runs only once on component mount
+    return(
+        <AsyncPaginate
+        placeholder ="Search for location"
+        debounceTimeout={600}
+        value={search}
+        onChange={handleOnChange}
+        loadOptions={loadOptions}
 
-  const loadOptions = (inputData) => {
-    const apiUrl = defaultLocation
-      ? `${GEO_API_URL}/cities?minPopulation=1000000&lat=${defaultLocation.latitude}&lon=${defaultLocation.longitude}`
-      : `${GEO_API_URL}/cities?minPopulation=1000000&namePrefix=${inputData}`;
-
-    return fetch(apiUrl, geoApiOptions)
-      .then((response) => response.json())
-      .then((response) => {
-        return {
-          options: response.data.map((city) => ({
-            value: `${city.latitude} ${city.longitude}`,
-            label: `${city.name}, ${city.countryCode}`,
-          })),
-        };
-      })
-      .catch((err) => console.log(err));
-  };
-
-  const handleOnChange = (searchData) => {
-    setSearch(searchData);
-    onSearchChange(searchData);
-  };
-
-  return (
-    <AsyncPaginate
-      placeholder="Search for location"
-      debounceTimeout={600}
-      value={search}
-      onChange={handleOnChange}
-      loadOptions={loadOptions}
-    />
-  );
-};
-
+        />
+    )
+}
 export default Search;
